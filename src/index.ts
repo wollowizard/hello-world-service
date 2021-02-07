e311c7d9f15a62bda281b4a51fcedbdf1f858d52
Alfredo Scaccialepre <alfredo.scaccialepre@sunrise.net>
2021-02-07 09:29:02 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+app.get( "/", ( req: express.Request, res: express.Response ) => {
+    res.json({"hello": "world"})
+} );
+
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
